bd1020c17c616cc68e09db33c0690c1268440519
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2025-01-17 21:21:05 -0600

    feat: vscode integration, fishnet networking

---
+++ Assets/Scripts/ConnectionHandler.cs
@@ -0,0 +1,59 @@
+using FishNet;
+using FishNet.Transporting;
+using UnityEngine;
+
+public enum ConnectionType
+{
+    Host,
+    Client
+}
+
+public class ConnectionHandler : MonoBehaviour
+{
+    public ConnectionType connectionType;
+
+    #if UNITY_EDITOR
+    private void OnDisable()
+    {
+        InstanceFinder.ClientManager.OnClientConnectionState -= OnClientConnectionState;
+    }
+
+    private void OnEnable()
+    {
+        InstanceFinder.ClientManager.OnClientConnectionState += OnClientConnectionState;
+    }   
+
+    private void OnClientConnectionState(ClientConnectionStateArgs args)
+    {
+        if (args.ConnectionState == LocalConnectionState.Stopping)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+    #endif
+
+    private void Start()
+    {
+
+        #if UNITY_EDITOR
+        if (ParrelSync.ClonesManager.IsClone())
+        {
+            InstanceFinder.ClientManager.StartConnection();
+        }
+        else
+        {
+            if (connectionType == ConnectionType.Host)
+            {
+                InstanceFinder.ServerManager.StartConnection();
+                InstanceFinder.ClientManager.StartConnection();
+            }
+            else
+            {
+                InstanceFinder.ClientManager.StartConnection();
+            }
+        }
+        #endif
+
+    }
+
+}
